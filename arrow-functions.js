681fd03b5dc9d99d1c1ca7a970788a44bdeeb6e5
Jamie McStay <jamiemcstay@hotmail.com>
2024-09-30 10:23:24 +0000

    Add info on arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumber2 = (a, b) => a + b;
+const addTwoNumbers = (a, b) => (a + b);
+
 // Implicit Returns
+const saySomething = message => console.log(mesage);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+
+console.log(returnMultipleLines());
